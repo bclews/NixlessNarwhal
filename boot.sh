d04282ed3c67d5043f12994dc01367218fe9e3b7
Bennett Clews <ben.clews@csiro.au>
2024-10-18 15:36:21 +1100

    wip

--- boot.sh
+++ boot.sh
@@ -27,7 +27,7 @@ sudo apt-get install -y git >/dev/null
 echo "Cloning NixlessNarwhal..."
 rm -rf ~/.local/share/NixlessNarwhal
 # git clone https://github.com/bclews/NixlessNarwhal.git ~/.local/share/NixlessNarwhal >/dev/null
-git clone https://github.com/bclews/NixlessNarwhal.git ~/.local/share/NixlessNarwhal >/dev/null
+git clone git@github.com:bclews/NixlessNarwhal.git ~/.local/share/NixlessNarwhal >/dev/null
 
 echo "Installation starting..."
 source ~/.local/share/NixlessNarwhal/install.sh
