6c0f80038d05a0134ee9471e8c6bec53623547c3
Bennett Clews <ben.clews@csiro.au>
2024-10-22 21:34:08 +1100

    wip

--- install.sh
+++ install.sh
@@ -1,9 +1,38 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+ascii_art='
+▄▀▀▄ ▀▄  ▄▀▀█▀▄   ▄▀▀▄  ▄▀▄  ▄▀▀▀▀▄     ▄▀▀█▄▄▄▄  ▄▀▀▀▀▄  ▄▀▀▀▀▄
+█  █ █ █ █   █  █ █    █   █ █    █     ▐  ▄▀   ▐ █ █   ▐ █ █   ▐
+▐  █  ▀█ ▐   █  ▐ ▐     ▀▄▀  ▐    █       █▄▄▄▄▄     ▀▄      ▀▄
+  █   █      █         ▄▀ █      █        █    ▌  ▀▄   █  ▀▄   █
+▄▀   █    ▄▀▀▀▀▀▄     █  ▄▀    ▄▀▄▄▄▄▄▄▀ ▄▀▄▄▄▄    █▀▀▀    █▀▀▀
+█    ▐   █       █  ▄▀  ▄▀     █         █    ▐    ▐       ▐
+▐        ▐       ▐ █    ▐      ▐         ▐
+ ▄▀▀▄ ▀▄  ▄▀▀█▄   ▄▀▀▄▀▀▀▄  ▄▀▀▄    ▄▀▀▄  ▄▀▀▄ ▄▄   ▄▀▀█▄   ▄▀▀▀▀▄
+█  █ █ █ ▐ ▄▀ ▀▄ █   █   █ █   █    ▐  █ █  █   ▄▀ ▐ ▄▀ ▀▄ █    █
+▐  █  ▀█   █▄▄▄█ ▐  █▀▀█▀  ▐  █        █ ▐  █▄▄▄█    █▄▄▄█ ▐    █
+  █   █   ▄▀   █  ▄▀    █    █   ▄    █     █   █   ▄▀   █     █
+▄▀   █   █   ▄▀  █     █      ▀▄▀ ▀▄ ▄▀    ▄▀  ▄▀  █   ▄▀    ▄▀▄▄▄▄▄▄▀
+█    ▐   ▐   ▐   ▐     ▐            ▀     █   █    ▐   ▐     █
+▐                                         ▐   ▐              ▐
+'
+
+echo -e "$ascii_art"
+echo "=> NixlessNarwhal is for fresh Ubuntu 22.04 installations only!"
+echo -e "\nBegin installation (or abort with ctrl+c)..."
+
+sudo apt-get update >/dev/null
+sudo apt-get install -y git >/dev/null
+
+echo "Cloning NixlessNarwhal..."
+rm -rf ~/.local/share/NixlessNarwhal
+# git clone https://github.com/bclews/NixlessNarwhal.git ~/.local/share/NixlessNarwhal >/dev/null
+git clone git@github.com:bclews/NixlessNarwhal.git ~/.local/share/NixlessNarwhal >/dev/null
+
+echo "Installation starting..."
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/NixlessNarwhal/install/check-version.sh
 
 # Install terminal tools
 source ~/.local/share/NixlessNarwhal/install/terminal.sh
-
