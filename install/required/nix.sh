fa3bd3d2681385ad697b87a90a23d597707f467c
Bennett Clews <ben.clews@csiro.au>
2024-11-07 21:51:55 +1100

    fix: update nix installation script
    - Set temporary directory for nix installation
    - Corrected path to nix profile script
    - Changed source command to use .zshrc instead of .bashrc
    - Cleaned up temporary directory after installation

--- install/required/nix.sh
+++ install/required/nix.sh
@@ -1,14 +1,19 @@
 # Yeah, so uh, not entirely nixless!
 # Some random packages require it.
 
+export TMPDIR=~/nix-install-temp
+mkdir -p $TMPDIR
+
 # Install Nix
 curl -L https://nixos.org/nix/install | sh
 
 # Source the Nix profile script
-. /home/your_user/.nix-profile/etc/profile.d/nix.sh
+. ~/.nix-profile/etc/profile.d/nix.sh
 
 # Source the bashrc to ensure the environment is updated
-source ~/.bashrc
+source ~/.zshrc
+
+rm -rf ~/nix-install-temp
 
 # Verify the installation
 nix --version
